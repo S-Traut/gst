f4bcbf7b731fd7a5f0d732757427939a5a8c2b01
Stéphane Traut <69320347+S-Traut@users.noreply.github.com>
2022-02-22 14:42:38 +0100

    Update impl.c

--- template/impl.c
+++ template/impl.c
@@ -1,9 +1,3 @@
-/*================================================================
-    * Copyright: 2020 John Jackson
-    * impl.c
-    // All necessary implementations will be here
-================================================================*/
-
 // Define gunslinger implementation
 #define GS_IMPL
 #include <gs/gs.h>
